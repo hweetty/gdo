be52ce54f200d5ccf9c98a282c45074e38d3e7d2
Jerry Yu <hweetty@gmail.com>
2019-03-03 16:08:28 -0800

    Add home shortcut

--- Source/iOS/gdo/AppDelegate.swift
+++ Source/iOS/gdo/AppDelegate.swift
@@ -15,9 +15,53 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	var window: UIWindow?
 
+    enum ShortcutIdentifier: String {
+        case toggle
+    }
+
+    private let shortcutUserInfoIconKey = "shortcutUserInfoIconKey"
+    private let toggleValue = "toggleValue"
+
+    func installShortcutActionsIfNeeded() {
+        if let shortcutItems = UIApplication.shared.shortcutItems, shortcutItems.isEmpty {
+            let toggleShortcut = UIMutableApplicationShortcutItem(type: ShortcutIdentifier.toggle.rawValue,
+                                                                  localizedTitle: "Toggle garage door",
+                                                                  localizedSubtitle: "",
+                                                                  icon: UIApplicationShortcutIcon(type: .home),
+                                                                  userInfo: nil)
+
+            UIApplication.shared.shortcutItems = [toggleShortcut]
+        }
+    }
+
+    private func toggleDoor() {
+        guard let environment = Environment.loadFromDiskIfPossible() else {
+            return
+        }
+
+        let commandData = CommandWrapper.serialize(type: .toggle, commandDetails: Dictionary<String, String>(), user: environment.user)
+        SocketHelper.send(data: commandData, to: environment.remoteHostName, port: environment.remotePort)
+    }
+
+    private func handleShortCutItem(_ shortCutItem: UIApplicationShortcutItem) {
+        switch shortCutItem.type {
+        case ShortcutIdentifier.toggle.rawValue:
+            toggleDoor()
+        default:
+            break
+        }
+    }
+
+    // Boilerplate
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-		// Override point for customization after application launch.
+        if let shortcutItem = launchOptions?[UIApplication.LaunchOptionsKey.shortcutItem] as? UIApplicationShortcutItem {
+            handleShortCutItem(shortcutItem)
+            return false
+        }
+
+        installShortcutActionsIfNeeded()
+
 		return true
 	}
 
@@ -43,6 +87,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		// Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 	}
 
-
+    /*
+     Called when the user activates your application by selecting a shortcut on the home screen, except when
+     application(_:,willFinishLaunchingWithOptions:) or application(_:didFinishLaunchingWithOptions) returns `false`.
+     You should handle the shortcut in those callbacks and return `false` if possible. In that case, this
+     callback is used if your application is already launched in the background.
+     */
+    func application(_ application: UIApplication, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Void) {
+        handleShortCutItem(shortcutItem)
+        completionHandler(true)
+    }
 }
 
